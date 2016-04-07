2506726d25bf66b61369fdad0420869b5f03029e
mbed_tw_hoehoe <null>
2016-04-07 11:54:08 +0000

    add BLE.

--- main.cpp
+++ main.cpp
@@ -24,7 +24,8 @@
 #include "mbed.h"
 #include "config.h"
 #include <stdio.h>
- 
+
+#include "BLE.h"
  
 #define DEG_TO_RAD(x) ( x * 0.01745329 )
 #define RAD_TO_DEG(x) ( x * 57.29578 )
@@ -54,19 +55,48 @@ struct MPU6050_DmpData {
     float roll, pitch, yaw;     // rad
 }dmpData;
  
-bool Init();
-void dmpDataUpdate();
- 
- 
-int main() {
-    MBED_ASSERT(Init() == true);
-    
-    while(1) {
-        dmpDataUpdate();
-    }
-}
- 
- 
+#define CONN_INTERVAL 25  /**< connection interval 20ms; in multiples of 0.125ms. (durationInMillis * 1000) / UNIT_0_625_MS; */
+#define CONN_SUP_TIMEOUT  8000 /**< Connection supervisory timeout (6 seconds); in multiples of 0.125ms. */
+#define SLAVE_LATENCY     0
+#define TICKER_INTERVAL   2.0f
+
+BLE   ble;
+
+static const char DEVICENAME[] = "TY51822r3";
+static volatile bool  triggerSensorPolling = false;
+
+static const char QUOTERNION_DESC[] = "quoternion";
+
+
+//const uint8_t MPU6050_adv_service_uuid[] = {
+//    0x9F,0xDF,0x32,0x83,
+//    0x90,0x49,
+//    0xCF,0x8D,
+//    0x5C,0x4D,    
+//    0x98,0xE7,0xE2,0x00,0x27,0x31
+//};
+
+static const uint8_t MPU6050_service_uuid[] = {
+    0x45,0x35,0x56,0x80,0x0F,0xD8,0x5F,0xB5,0x51,0x48,0x30,0x27,0x06,0x9B,0x3F,0xD9
+};
+
+static const uint8_t MPU6050_dmp_Characteristic_uuid[] = {
+    0x45,0x35,0x56,0x81,0x0F,0xD8,0x5F,0xB5,0x51,0x48,0x30,0x27,0x06,0x9B,0x3F,0xD9
+};
+
+
+uint8_t quoternionPayload[sizeof(float)*4] = {0,};
+
+GattAttribute       quoternionAttr (BLE_UUID_DESCRIPTOR_CHAR_USER_DESC, (uint8_t *)QUOTERNION_DESC, sizeof(QUOTERNION_DESC), sizeof(QUOTERNION_DESC));
+GattAttribute       *quoternionAttrs[] = { &quoternionAttr, };
+GattCharacteristic  quoternionChar (MPU6050_dmp_Characteristic_uuid,
+                                        quoternionPayload, (sizeof(float) * 10), (sizeof(float) * 10),
+                                        GattCharacteristic::BLE_GATT_CHAR_PROPERTIES_READ, quoternionAttrs, 1);
+
+GattCharacteristic *ControllerChars[] = { &quoternionChar, };
+GattService         MPU6050DMPService(MPU6050_service_uuid, ControllerChars, sizeof(ControllerChars) / sizeof(GattCharacteristic *));
+
+
 bool Init() {
     pc.baud(PC_BAUDRATE);
         
@@ -124,6 +154,13 @@ void dmpDataUpdate() {
         mpu.dmpGetQuaternion(&dmpData.q, fifoBuffer);
         if ( snprintf( snprintf_buffer, snprintf_buffer_size, "Quaternion : w=%f, x=%f, y=%f, z=%f\n", dmpData.q.w, dmpData.q.x, dmpData.q.y, dmpData.q.z ) < 0 ) return;
         pc.puts(snprintf_buffer);
+        
+        memcpy(quoternionPayload+sizeof(float)*0, &dmpData.q.w, sizeof(float));
+        memcpy(quoternionPayload+sizeof(float)*1, &dmpData.q.x, sizeof(float));
+        memcpy(quoternionPayload+sizeof(float)*2, &dmpData.q.y, sizeof(float));
+        memcpy(quoternionPayload+sizeof(float)*3, &dmpData.q.y, sizeof(float));
+        
+        ble.updateCharacteristicValue(quoternionChar.getValueAttribute().getHandle(), quoternionPayload, sizeof(quoternionPayload));    //Mod
     #endif
         
     #ifdef OUTPUT_EULER
@@ -170,3 +207,97 @@ void dmpDataUpdate() {
         pc.printf("\n");
     }
 }
+
+void disconnectionCallback(const Gap::DisconnectionCallbackParams_t *params)    // Mod
+{
+    
+    //DEBUG("Disconnected handle %u, reason %u\n", handle, reason);
+    //DEBUG("Restarting the advertising process\n\r");
+
+    ble.startAdvertising();
+}
+
+void connectionCallback(const Gap::ConnectionCallbackParams_t *params)
+{
+
+    //DEBUG("connected. Got handle %u\r\n", handle);
+
+    /*******************************************************************************/
+    /*  CentralがMacOS X の時 connection intervalを設定する場合は                      */
+    /*  nRF51822 -> projectconfig.h -> GAP ->                                      */
+    /*  CFG_GAP_CONNECTION_MIN_INTERVAL_MS / CFG_GAP_CONNECTION_MAX_INTERVAL_MSを  */
+    /*  直接編集すること                                                             */
+    /******************************************************************************/
+        //Gap::Handle_t handle;
+        Gap::ConnectionParams_t gap_conn_params;
+        gap_conn_params.minConnectionInterval        = Gap::MSEC_TO_GAP_DURATION_UNITS(CONN_INTERVAL);
+        gap_conn_params.maxConnectionInterval        = Gap::MSEC_TO_GAP_DURATION_UNITS(CONN_INTERVAL);
+        gap_conn_params.connectionSupervisionTimeout = Gap::MSEC_TO_GAP_DURATION_UNITS(CONN_SUP_TIMEOUT);
+        gap_conn_params.slaveLatency                 = SLAVE_LATENCY;
+        
+    if (ble.updateConnectionParams(params->handle, &gap_conn_params) != BLE_ERROR_NONE) {
+        //DEBUG("failed to update connection paramter\r\n");
+    }
+}
+
+void timeoutCallback(const Gap::TimeoutSource_t source)
+{
+    //DEBUG("TimeOut\n\r");
+    //DEBUG("Restarting the advertising process\n\r");    
+
+    ble.startAdvertising();
+}
+
+void bleInit(){
+    ble.init();
+    ble.onDisconnection(disconnectionCallback);
+    ble.onConnection(connectionCallback);
+    //ble.onDataWritten(writtenCallback);
+    ble.onTimeout(timeoutCallback);
+
+    /* setup device name */
+    ble.setDeviceName((const uint8_t *)DEVICENAME);
+    
+    /* setup advertising */
+    ble.accumulateAdvertisingPayload(GapAdvertisingData::BREDR_NOT_SUPPORTED  | GapAdvertisingData::LE_GENERAL_DISCOVERABLE);
+    ble.setAdvertisingType(GapAdvertisingParams::ADV_CONNECTABLE_UNDIRECTED);
+    ble.accumulateAdvertisingPayload(GapAdvertisingData::COMPLETE_LOCAL_NAME, (const uint8_t *)DEVICENAME, sizeof(DEVICENAME));
+    ble.accumulateAdvertisingPayload(GapAdvertisingData::COMPLETE_LIST_128BIT_SERVICE_IDS,
+                                    (const uint8_t *)MPU6050_service_uuid, sizeof(MPU6050_service_uuid));
+                                    //(const uint8_t *)MPU6050_adv_service_uuid, sizeof(MPU6050_adv_service_uuid));
+
+    ble.setAdvertisingInterval(160); /* 100ms; in multiples of 0.625ms. */
+    ble.startAdvertising();
+
+    ble.addService(MPU6050DMPService);
+}
+
+void periodicCallback(void)
+{
+    //oneSecondLed = !oneSecondLed; /* Do blinky on LED1 while we're waiting for BLE events */
+
+    /* Note that the periodicCallback() executes in interrupt context, so it is safer to do
+     * heavy-weight sensor polling from the main thread. */
+    triggerSensorPolling = true;
+}
+
+
+int main() {
+    MBED_ASSERT(Init() == true);
+    
+    Ticker ticker;
+    ticker.attach(periodicCallback, 0.1f);
+    
+    bleInit();
+    
+    while(true) {
+        if (triggerSensorPolling && ble.getGapState().connected) {
+            triggerSensorPolling = false;
+            dmpDataUpdate();
+        } else {
+            ble.waitForEvent();
+        }
+
+    }
+}
+ 
